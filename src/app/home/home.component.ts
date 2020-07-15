260f8c0c3ca45dd504cdbf62aed920eed0f66f49
vishwaaas@gmail.com <vishwaaas@gmail.com>
2020-07-14 20:41:57 -0400

    final

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,31 +10,36 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 export class HomeComponent implements OnInit {
 
-  algorithm : string;
+  algorithm: string;
   data: string;
   publicExp: string;
   publicKey: string;
   base64Encode: string;
+  response : string;
 
-  constructor(private http : HttpClient) { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
   onSubmit(f: NgForm) {
-    let headers = new HttpHeaders({ 'Content-Type': 'application/json', 
-    'Access-Control-Allow-Origin' : '*',
-    'Access-Control-Allow-Methods' : '*',
-    'Access-Control-Allow-Headers' : '*'});
-    let options = { headers: headers };
-  
-    console.log("Data "+this.algorithm + this.data + this.publicExp + this.publicKey + this.base64Encode);
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': 'http://192.168.0.18:8080',
+      'Access-Control-Allow-Methods': 'POST',
+      'Access-Control-Allow-Headers': '*',
+      'Access-Control-Allow-Credentials' : 'true'
+    });
+    
+    let signInfo = {
+      algorithm: this.algorithm, data: this.data, publicExp: this.publicExp,
+      publicKey: this.publicKey, base64Encode: this.base64Encode
+    };
 
-    let signInfo = { algorithm : this.algorithm, data: this.data, publicExp : this.publicExp, 
-      publicKey: this.publicKey, base64Encode : this.base64Encode };
-
-    return this.http.post("http://192.168.0.18:8080/validate", signInfo, options).subscribe(data => {
+    return this.http.post("http://192.168.0.18:8080/validate", signInfo, {headers : headers, responseType:'text'})
+    .subscribe((data:string) => {
       console.log(data);
-  });
-}
+      this.response = data;
+    });
+  }
 }
